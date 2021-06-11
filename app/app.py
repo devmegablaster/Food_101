56ae3acffc7602e03a21e6e8fb86724e85d3afbd
MEGA-BLASTER2004 <ojastyagi@outlook.com>
2021-06-11 18:28:19 +0530

    added structure

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Food:101 :hamburger:")
-st.markdown('''Hey There!
+st.markdown('''Hey There! <Br>
 Food 101 is a *Deep Learning Model* trained to predict what kind of food is present in an image''')
