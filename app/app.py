a4176041a267f62d7744d717bf8ac4b3ab468725
MEGA-BLASTER2004 <ojastyagi@outlook.com>
2021-06-11 18:30:43 +0530

    added structure

--- app/app.py
+++ app/app.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title("Food:101 :hamburger:")
 st.markdown('''## Hey There! 
 \n
-Food 101 is a *Deep Learning Model* trained to predict what kind of food is present in an image''')
+Food 101 is a *Deep Learning Model* trained to predict what kind of food is present in an image
+\n
+As the name `Food 101` suggests, this model can predict ** 101 different food categories! **''')
