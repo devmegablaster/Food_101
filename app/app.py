6e7c627f1c7a7092676d48f3034886640b13a4c0
MEGA-BLASTER2004 <ojastyagi@outlook.com>
2021-06-11 18:31:31 +0530

    added structure

--- app/app.py
+++ app/app.py
@@ -5,4 +5,6 @@ st.markdown('''## Hey There!
 \n
 Food 101 is a *Deep Learning Model* trained to predict what kind of food is present in an image
 \n
-As the name `Food 101` suggests, this model can predict ** 101 different food categories! **''')
+As the name `Food 101` suggests, this model can predict ** 101 different food categories! **
+\n
+### Try out yourself!''')
